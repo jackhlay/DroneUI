76e5ef8b0f7e46659a4032fcccfb3e3b64946005
JackLay1 <42285595+JackLay1@users.noreply.github.com>
2023-11-13 14:22:26 -0700

    Python now
    we're doing python now

--- src/Controller.java
+++
@@ -1,8 +0,0 @@
-public class Controller {
-    //Pull all the strings
-    public static void main(String[] args) {
-        View frame = new View();
-        frame.display();
-
-    }
-}
--- src/Model.java
+++
@@ -1,7 +0,0 @@
-public class Model {
-    //Handle data
-    String takeOffLat;
-    String getTakeOffLong;
-    String newPointLat;
-    String newPointLong;
-}
